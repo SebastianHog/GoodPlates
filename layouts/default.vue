950bf6400388cc8cf52752a42044676702402f38
sebastian Högsander <seblhog@gmail.com>
2025-01-07 22:42:15 +0100

    installed scss for variables and stuff, started thinking about responsiveness but probably wont focus on that for now

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,43 +1,14 @@
 <template>
   <div class="default-layout-container">
     <div class="sidebar">
-      <img src="@/assets/logo/GoodPlatesLogo.png" alt="goodplates logo" class="logo-img" />
+      <div class="logoTitleImage">
+        <img src="@/assets/logo/GoodPlatesLogo.png" alt="goodplates logo" class="logo-img" />
+        <h1 class="logoTitle">GoodPlates</h1>
+      </div>
+      <div class="seperator"></div>
     </div>
     <Nuxt />
   </div>
 </template>
 
-<style>
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: sans-serif;
-  font-weight: 400;
-}
-
-body {
-  background-color: #ACFFA2;
-}
-
-.default-layout-container {
-  display: flex;
-  flex-direction: row;
-}
-
-.sidebar {
-  background: rgba(0, 0, 0, 0.03);
-  width: 15rem;
-  height: 100vh;
-
-  display: flex;
-  justify-content: center;
-
-  .logo-img {
-    margin-top: 2rem;
-    width: 10rem;
-    height: 10rem;
-
-  }
-}
-</style>
+<style src="./default_styles.scss" lang="scss"></style>
